a20bf192f987714b9c203f78f379a9d4cebb08e8
Vinitthakkar45 <45vinitthakkar@gmail.com>
2024-11-01 22:31:21 +0530

    add dynamical memes by fetch and useEffect Hook

--- Meme Generator/src/Components/Meme.jsx
+++ Meme Generator/src/Components/Meme.jsx
@@ -1,66 +1,68 @@
-import memesData from "../data/memesData.js"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Meme() {
-
     const [meme, setMeme] = useState({
-        topText: "",
-        bottomText: "",
-        randomImage: "http://i.imgflip.com/1bij.jpg" 
-    })
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
-    const [top, setTop] = useState("One does not simply")
-    const [bottom, setBottom] = useState("Walk into Mordor")
-    
-    
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        randomImage: "/images/image.png",
+    });
+    const [allMemeImages, setAllMemeImages] = useState([]);
+
     function getMemeImage() {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
-        setMeme(prevMeme => ({
+        const memesArray = allMemeImages;
+        const randomNumber = Math.floor(Math.random() * memesArray.length);
+        const url = memesArray[randomNumber].url;
+        setMeme((prevMeme) => ({
             ...prevMeme,
-            randomImage: url
-        }))
-        
+            randomImage: url,
+        }));
     }
-    
-    function handleTop(event) {
-       setTop(event.target.value)
+
+    useEffect(() => {
+        console.log("jk")
+        fetch("https://api.imgflip.com/get_memes")
+            .then((response) => response.json())
+            .then((data) => {
+                setAllMemeImages(data.data.memes);
+            });
+    }, []);
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setMeme((prevMeme) => ({
+            ...prevMeme,
+            [name]: value,
+        }));
     }
-    
-    function handleBottom(event) {
-        setBottom(event.target.value)
-    }
-    
+
     return (
         <main>
             <div className="form">
-                <input 
+                <input
                     type="text"
                     placeholder="Top text"
                     className="form--input"
-                    value={top}
-                    onChange={handleTop}
+                    value={meme.topText}
+                    onChange={handleChange}
+                    name="topText"
                 />
-                <input 
+                <input
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
-                    value={bottom}
-                    onChange={handleBottom}
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                    name="bottomText"
                 />
-                <button 
-                    className="form--button"
-                    onClick={getMemeImage}
-                >
+                <button className="form--button" onClick={getMemeImage}>
                     Get a new meme image 🖼
                 </button>
             </div>
             <div className="meme">
                 <img src={meme.randomImage} className="meme--image" />
-                <h2 className="meme--text top">{top}</h2>
-                <h2 className="meme--text bottom">{bottom}</h2>
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
         </main>
-    )
+    );
 }
