9a3d34c36ef163af256efe11266c90f6cdb52e60
Vinitthakkar45 <45vinitthakkar@gmail.com>
2025-01-18 17:47:41 +0530

    feat : Home and About Van Life

--- React Router 6/van-life/src/App.jsx
+++ React Router 6/van-life/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
-
   return (
-    <div> ghjk</div>
-  )
+    <BrowserRouter>
+      <header>
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
